42863827c422d32e3251695c583f0fb1467468c1
Julia Mazzoni <juliarmazzoni@gmail.com>
2024-11-28 12:55:02 +1100

    adding colours to the tabs

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,38 +2,53 @@ import { useEffect, useState } from "react"
 
 export const Dashboard = () => {
 
-  const [timeTracking, setTimeTracking] = useState([{title: 'Work', current: 0, previous: 0}, {title:'Play', current: 0, previous: 0}, {title:'Study', current: 0, previous: 0}, {title:'Exercise', current: 0, previous: 0}, {title:'Social', current: 0, previous: 0}, {title:'Self Care', current: 0, previous: 0}])
-
   const [data, setData] = useState([])
+  const [timeTracking, setTimeTracking] = useState([])
+  const [activeTab, setActiveTab] = useState('')
+
 
   useEffect(() => {
-
     fetch('/data.json')
       .then(response => response.json())
-      .then(data => setData(data))
+      .then(data => {
+        const backgroundColors = ['orange', 'blue', 'pink', 'green', 'purple', 'yellow']
+        const icons = []
+
+        const updatedData = data.map((item, index) => {
+          return {...item, class: backgroundColors[index]}
+        })
+
+        const initialState = data.map((item, index) => {
+          return {title: item.title, current: 0, previous: 0,  class: backgroundColors[index]}
+        })
+
+        setData(updatedData)
+        setTimeTracking(initialState)
+
+      })
   }, [])
 
 
   const handleDailyClick = () => {
-    const dailyData = data.map(item => {
-      return {title: item.title, current: item.timeframes.daily.current, previous: 'Yesterday - ' + item.timeframes.daily.previous}
-      
+    setActiveTab('daily')
+    let dailyData = data.map(item => {
+      return {title: item.title, current: item.timeframes.daily.current, previous: 'Yesterday - ' + item.timeframes.daily.previous, class: item.class}
     })
     setTimeTracking(dailyData)
   }
 
   const handleWeeklyClick = () => {
+    setActiveTab('weekly')
     const weeklyData = data.map(item => {
-      return {title: item.title, current: item.timeframes.weekly.current, previous: 'Last Week - ' + item.timeframes.weekly.previous}
-      
+      return {title: item.title, current: item.timeframes.weekly.current, previous: 'Last Week - ' + item.timeframes.weekly.previous,  class: item.class}
     })
     setTimeTracking(weeklyData)
   }
 
   const handleMonthlyClick = () => {
+    setActiveTab('monthly')
     const monthlyData = data.map(item => {
-      return {title: item.title, current: item.timeframes.monthly.current, previous: 'Last Month - ' + item.timeframes.monthly.previous}
-      
+      return {title: item.title, current: item.timeframes.monthly.current, previous: 'Last Month - ' + item.timeframes.monthly.previous,  class: item.class}
     })
     setTimeTracking(monthlyData)
   }
@@ -47,14 +62,16 @@ export const Dashboard = () => {
 
           <div className="jeremy-profile">
             <img className="jeremy-image" src="/images/image-jeremy.png" alt="" />
-            <h4>Report for</h4>
-            <h1>Jeremy Robson</h1>
+            <div className="jeremy-text-wrapper">
+              <h4>Report for</h4>
+              <h1>Jeremy Robson</h1>
+            </div>
           </div>
 
-          <div className="tabs">
-            <h3 onClick={handleDailyClick}>Daily</h3>
-            <h3 onClick={handleWeeklyClick}>Weekly</h3>
-            <h3 onClick={handleMonthlyClick}>Monthly</h3>
+          <div className="tabs-container">
+            <h3 className={activeTab === 'daily' ? 'active-tab' : 'tabs'} onClick={handleDailyClick}>Daily</h3>
+            <h3 className={activeTab === 'weekly' ? 'acitve-tab' : 'tabs'} onClick={handleWeeklyClick}>Weekly</h3>
+            <h3 className={activeTab === 'monthly' ? 'acitve-tab' : 'tabs'} onClick={handleMonthlyClick}>Monthly</h3>
           </div>
           
 
@@ -62,10 +79,12 @@ export const Dashboard = () => {
 
       {timeTracking.map((item, index) => {
         return (
-          <div className="cell">
-            <h5>{item.title}</h5>
-            <h2>{item.current}hrs</h2>
-            <h6>{item.previous}hrs</h6>
+          <div key={index} className={`${item.class} cell-wrapper`} >
+            <div className="cell">
+              <h5>{item.title}</h5>
+              <h2>{item.current}hrs</h2>
+              <h6>{item.previous}hrs</h6>
+            </div>
           </div>
         )
       })}
